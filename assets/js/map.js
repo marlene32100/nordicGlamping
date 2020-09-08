230c4f90e13de58de37cf5c6e538b07a2598c8bc
marlene32100 <katia.debon@hotmail.it>
2020-09-08 18:53:07 +0000

    Added map and recommended by section

---
+++ assets/js/map.js
@@ -0,0 +1,10 @@
+function initMap() {
+        
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 59.759372,
+                    lng: 17.772206
+                },
+            });
+        }
