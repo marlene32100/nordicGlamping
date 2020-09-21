d94a0258caa57e2ba6ce6e4f51d616f023526f44
marlene32100 <katia.debon@hotmail.it>
2020-09-21 14:46:05 +0000

    Added menu.js

---
+++ assets/js/menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $("#navbarMenuLink").click(function(){
+       $("#menu-expanded").css("visibility", "visible");
+   });
+
+   $(".link-item").click(function(){
+       $("#menu-expanded").css("visibility", "hidden");
+   });
+
+
+   $(".close-icon").click(function(){
+       $("#menu-expanded").css("visibility", "hidden");
+   });
+});
