98ed0aa25a80022da5dbebdc9233883584102e64
marlene32100 <katia.debon@hotmail.it>
2020-09-24 20:43:54 +0000

    Added links in infowinfow, plus styling on hover

--- assets/js/map.js
+++ assets/js/map.js
@@ -13,94 +13,104 @@
         title:"Island Lodge",
         position: {lat: 59.370024, lng:18.379133},
         map: map,
-        image: "assets/images/islandlodge.jpg"
+        image: "assets/images/islandlodge.jpg",
+        url: "https://islandlodge.se/Island_Lodge/HOME.html"
         },
 
         {
         title: "Ice Hotel",
         position: {lat: 67.858899, lng: 20.527139},
         map: map,
-        image: "assets/images/ice.jpg"
+        image: "assets/images/ice.jpg",
+        url: "https://www.icehotel.com/"
         },
 
         {
         title:"Tree Hotel",
         position: {lat: 66.072939, lng: 20.981868},
         map: map,
-        image: "assets/images/treehotel.jpg"
+        image: "assets/images/treehotel.jpg",
+        url: "https://www.treehotel.se/en/"
         },
 
         {
         title:"Narvik Adventures",
         position: {lat: 68.430937, lng:17.436482},
         map: map,
-        image: "assets/images/narvik.jpg"
+        image: "assets/images/narvik.jpg",
+        url: "https://narvikadventures-com.webnode.com/"
         },
 
         {
         title: "Aurora Glamping Jeris",
         position: {lat: 67.952358, lng: 23.925108},
         map: map,
-        image: "assets/images/jeris.jpg"
+        image: "assets/images/jeris.jpg",
+        url: "https://harriniva.fi/holiday-destination/jeris/aurora-glamping-jeris/"
         },
 
         {
         title:"Torassieppi Jerisjärvi",
         position: {lat: 67.952380, lng: 23.925098},
         map: map,
-        image: "assets/images/torassieppi.jpg"
+        image: "assets/images/torassieppi.jpg",
+        url: "https://harriniva.fi/holiday-destination/torassieppi/auroraclamping_torassieppi/"
         },
 
         {
         title:"Kakslauttanen Igloo",
         position: {lat: 68.334263, lng:27.334750},
         map: map,
-        image: "assets/images/kakslauttanen.jpg"
+        image: "assets/images/kakslauttanen.jpg",
+        url: "https://www.kakslauttanen.fi/"
         },
 
         {
         title: "Levin Igloo",
         position: {lat: 67.787191, lng: 24.891061},
         map: map,
-        image: "assets/images/levin.jpg"
+        image: "assets/images/levin.jpg",
+        url: "https://leviniglut.net/glass-igloos-lapland-finland/"
         },
 
         {
         title:"Reykjaviks Domes",
         position: {lat: 64.146665, lng:  -21.809352},
         map: map,
-        image: "assets/images/domes.jpg"
+        image: "assets/images/domes.jpg",
+        url: "https://www.reykjavikdomes.com/"
         },
 
         {
         title:"Buubble",
         position: {lat: 64.165032, lng: -20.501192},
         map: map,
-        image: "assets/images/buubble.jpg"
+        image: "assets/images/buubble.jpg",
+        url: "https://www.buubble.com/"
         }
         ];
 
         var infoWindowContent = [
-            ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[0].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[0].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[1].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[1].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[2].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[2].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[3].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[3].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[4].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[4].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[5].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[5].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[6].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[6].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[7].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[7].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[8].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[8].title+'</h1>'+'</div>'+'</div>'],
-  ['<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[9].image+'" alt="Card image cap">'
-  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[9].title+'</h1>'+'</div>'+'</div>']
+            ['<a href="'+locations[0].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[0].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[0].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[1].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[1].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[1].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[2].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[2].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[2].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[3].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[3].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[3].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[4].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[4].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[4].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[5].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[5].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[5].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[6].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[6].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[6].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[7].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[7].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[7].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[8].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[8].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[8].title+'</h1>'+'</div>'+'</div>'+'</a>'],
+  ['<a href="'+locations[9].url+'" target="_blank" class="link-map">'+'<div class="card" style="width: 20rem;">'+'<img class="card-img-top" src="'+locations[9].image+'" alt="Card image cap">'
+  +'<div class="card-body">'+'<h1 class="card-text map-title">'+locations[9].title+'</h1>'+'</div>'+'</div>'+'</a>']
         ];
 
 
